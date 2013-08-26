b05a62c95a6b9e7cf6de23587bd73d624de45ce1
Nabil Adouani <nabil.adouani@gmail.com>
2013-08-27 00:21:50 +0200

    move icon files to use them in android app
    TODO: provide HD icons

--- Resources/ui/common/ApplicationTabGroup.js
+++ Resources/ui/common/ApplicationTabGroup.js
@@ -7,7 +7,7 @@ function ApplicationTabGroup(){
 	var citiesWindow = new CitiesWindow(); 
 	var baggersTab = Ti.UI.createTab({
 		title: 'Baggers',
-		icon: 'icons/baggers.png',
+		icon: '/images/icons/baggers.png',
 		window: citiesWindow
 	});
 	citiesWindow.containingTab = baggersTab;
@@ -17,7 +17,7 @@ function ApplicationTabGroup(){
 	var locationsWindow = new LocationsWindow();
 	var locationsTab = Ti.UI.createTab({
 		title : 'Locations',
-		icon: 'icons/locations.png',
+		icon: '/images/icons/locations.png',
 		window: locationsWindow
 	});
 	locationsWindow.containingTab = locationsTab;
